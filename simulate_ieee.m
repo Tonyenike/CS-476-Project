b01c3089a5c8d521b0694637e5ea3cb0b2c592e4
Tonyenike <32174978+Tonyenike@users.noreply.github.com>
2019-02-22 14:20:12 -0800

    Update simulate_ieee.m

--- simulate_ieee.m
+++ simulate_ieee.m
@@ -11,16 +11,22 @@ function simulate_project(n)
 
     p = 10; % Number of software downloads
     b = 10; % Number of block in the chain
+    avg_items = 10; % Number of IoT in gateway.
+    
+    nodes = 100;
     
     avg_software_package_size = 256; % Average packet size is 256 bits
     
+    service_rate = 20; % Service rate is 20 bits per second
+    
     for i = 1:n
         for k = 1:p
-            S(k) = round(poissnd(avg_software_package_size)); % Poisson distribution
+            S(k) = round(poissrnd(avg_software_package_size)); % Poisson distribution
             for j = 1:n
-                C(k, j) = S(k) * S(k); % Polynomial-time hash to verify each block. We chose n^2.
+                C(k, j) = (S(k) / service_rate) * (S(k) / service_rate); % Polynomial-time hash to verify each block. We chose n^2.
             end
+            download_time = S(k) / service_rate; % time to download the firmware once we have verified the block. Assume that executing smart contract time is 0.
+            routing_time = poissrnd(avg_items);
         end
     end
-
 end
