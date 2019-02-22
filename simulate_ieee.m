501ad2f4d98c343091320543302d38279cc71a0d
Tonyenike <32174978+Tonyenike@users.noreply.github.com>
2019-02-22 14:33:16 -0800

    Update simulate_ieee.m

--- simulate_ieee.m
+++ simulate_ieee.m
@@ -22,13 +22,15 @@ function simulate_project(n)
     
     service_rate = 20; % Service rate is 20 bits per second
     
+    C = zeros(1, p);
+    G = zeros(1, p);
+    S = zeros(1, p);
+    
     for i = 1:n
         for k = 1:p
             S(k) = round(poissrnd(avg_software_package_size)); % Poisson distribution
             G(k) = round(poissrnd(avg_contract_creation));
-            for j = 1:n
-                C(k, j) = (G(k) / service_rate) * (G(k) / service_rate); % Polynomial-time hash to create each block. We chose n^2.
-            end
+            C(k) = (G(k) / service_rate) * (G(k) / service_rate); % Polynomial-time hash to create each block. We chose n^2.
             download_time = S(k) / service_rate; % time to download the firmware once we have verified the block. Assume that executing smart contract time is 0.
         end
     end
